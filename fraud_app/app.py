8c4c91869b08e93995dc1f1d332076a3453af0c1
Ubuntu <ubuntu@ip-172-31-25-191.us-west-2.compute.internal>
2020-05-29 19:33:27 +0000

    Start app

--- fraud_app/app.py
+++ fraud_app/app.py
@@ -1,17 +1,19 @@
-import numpy as np
-from sklearn.ensemble import GradientBoostingClassifier
-import pickle
-
-
+import pandas as pd
+import psycopg2 as pg 
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('junyi.html')
+    return render_template('fraud.html', tables)
 
-
+@app.route("/table")
+def show_table():
+    data = pd.DataFrame({'a': 1, 'b': 2, 'c': 3}, index=['Time', 'Space', "Everything"])
+table = data.to_html()
+    return render_template('fraud.html', tables =table)
+'''
 @app.route('/solve', methods=['POST'])
 def solve():
     user_data = request.json
@@ -25,7 +27,7 @@ def _return_prediction(a, b, c, d):
     X = np.array([[a,b,c,d]])
     prediction = gbc.predict_proba(X)[0][1]
     return prediction.round(2) * 100
-    
+'''    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
